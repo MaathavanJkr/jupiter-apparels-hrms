63e35fc91018d31410069be15f7e32892afb4355
sahan974 <113873628+sahan974@users.noreply.github.com>
2024-10-01 18:16:25 +0530

    Update app.ts
    Resolved the conflicts.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,9 @@
 import express from 'express';
 import organizationRoutes from './routes/organization.route';
 import userRoutes from './routes/user.route';
+import branchRoutes from './routes/branch.route';
+import departmentRoutes from "./routes/department.route";
+import employeeRoutes from "./routes/employee.route";
 import emergencyContactRoutes from './routes/emergencyContact.route';
 import employeeDependentRoutes from './routes/dependent.route';
 import employmentStatusRoutes from './routes/employmentStatus.route';
@@ -14,6 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 //Settiing up Routes
 app.use('/organization', organizationRoutes);
 app.use('/user',userRoutes);
+app.use('/branch', branchRoutes);
+app.use('/department', departmentRoutes);
+app.use('/employee', employeeRoutes);
 app.use('/emergencycontact',emergencyContactRoutes);
 app.use('/dependent',employeeDependentRoutes);
 app.use('/employmentstatus',employmentStatusRoutes);
