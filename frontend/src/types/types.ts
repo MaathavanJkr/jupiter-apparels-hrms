d799252e90b13bd9625e920ceab4bfa178dd92ab
unknown <prathhp231@gmail.com>
2024-10-08 18:51:58 +0530

    button fix

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -107,9 +107,13 @@ export interface UserInfo {
     marital_status: string,
     email: string,
     nic: string,
+    address:string,
     department_name: string,
     branch_name: string,
     job_title: string,
     employment_status: string,
     contact_number: string,
+    pay_grade_name : string,
+    supervisor_id: string
+
 }
