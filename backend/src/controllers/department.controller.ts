ab401eb7d8b89efff1f5821c1f55909467da6510
Chamindi M.G.H.A <160192961+HiruniAyeshaChamindi@users.noreply.github.com>
2024-10-10 22:02:36 +0530

    Update department.controller.ts

--- backend/src/controllers/department.controller.ts
+++ backend/src/controllers/department.controller.ts
@@ -1,73 +1,99 @@
 // src/controllers/userController.ts
 import { Request, Response } from "express";
-import db from "../database/database";
-import { Department } from "../models/department.model";
-import { ResultSetHeader } from "mysql2";
+import {
+    Department,
+    createDepartmentModel,
+    deleteDepartmentModel,
+    getAllDepartmentsModel,
+    getDepartmentByIDModel,
+    updateDepartmentModel,
+}from "../models/department.model"
 
 export const createDepartment = async (req: Request, res: Response) => {
     const { name } = req.body;
-    try {
-        const [result] = await db
-            .promise()
-            .query(
-                "INSERT INTO departments (name) VALUES (?)",
-                [name]
-            );
-        res.status(201).json({ id: (result as ResultSetHeader).insertId, message: "Department created" });
-    } catch (error) {
-        res.status(500).json({ error: "Database query failed", message: error });
-    }
-}
+    
+    if (!name ) {
+        return res.status(400).json({ error: "Missing required fields" });
+      }
+    
+      const department: Department = {
+        name: name as string,
+      } as Department;
+    
+      await createDepartmentModel(department)
+        .then((result) => {
+          return res.status(201).json(result);
+        })
+        .catch((error) => {
+          return res.status(500).json({ error });
+        });
+};
 
-export const getDepartments = async (req: Request, res: Response) => {
-    try {
-        const [departments] = await db
-            .promise()
-            .query<Department[]>("SELECT * FROM departments");
-        res.status(200).json(departments);
-    } catch (error) {
-        res.status(500).json({ error: "Database query failed", message: error });
-    }
+export const getAllDepartments = async (req: Request, res: Response) => {
+    await getAllDepartmentsModel()
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((error) => {
+      return res.status(500).json({ error });
+    });
 };
 
 export const getDepartmentByID = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    try {
-        const [departments] = await db
-            .promise()
-            .query<Department[]>("SELECT * FROM departments WHERE id = ?", id);
+    const { id } = req.params;
 
-        res.status(200).json(departments[0]);
-    } catch (error) {
-        res.status(500).json({ error: "Database query failed", message: error });
-    }
-}
+  await getDepartmentByIDModel(id)
+    .then((result) => {
+      return res.status(200).json(result);
+    })
+    .catch((error) => {
+      return res.status(500).json({ error });
+    });
+};
 
 export const updateDepartment = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const { name } = req.body;
-    try {
-        await db
-            .promise()
-            .query(
-                "UPDATE departments SET name = ? WHERE id = ?",
-                [name, id]
-            );
-        res.status(200).json({ message: "Department updated" });
-    } catch (error) {
-        res.status(500).json({ error: "Database query failed", message: error });
-    }
-}
+    const { id } = req.params;
+  const { name } = req.body;
+
+  await getDepartmentByIDModel(id)
+    .then(async (result) => {
+      if (!result.data) {
+        return res.status(404).json(result);
+      }
+      const department = result.data;
+      if (name) department.name = name;
+      
+      await updateDepartmentModel(department)
+        .then((result) => {
+          return res.status(200).json(result);
+        })
+        .catch((error) => {
+          return res.status(500).json({ error });
+        });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error });
+    });
+};
 
 
 export const deleteDepartment = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    try {
-        const [result] = await db
-            .promise()
-            .query("DELETE FROM departments WHERE id = ?", id);
-        res.status(200).json({ id: result, message: `Department with id: ${id} deleted` });
-    } catch (error) {
-        res.status(500).json({ error: "Database query failed", message: error });
-    }
-}
+    const { id } = req.params;
+
+  await getDepartmentByIDModel(id)
+    .then(async (result) => {
+      if (!result.data) {
+        return res.status(404).json(result);
+      }
+      await deleteDepartmentModel(id)
+        .then((result) => {
+          return res.status(200).json(result);
+        })
+        .catch((error) => {
+          return res.status(500).json({ error });
+        });
+    })
+    .catch((error) => {
+      return res.status(500).json({ error });
+    });
+};
