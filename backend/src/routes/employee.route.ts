12406f9bc3228833c094354eaa70ad4044755f38
MaathavanJkr <maathumaathavan@gmail.com>
2024-10-19 00:20:52 +0530

    fixes

--- backend/src/routes/employee.route.ts
+++ backend/src/routes/employee.route.ts
@@ -1,12 +1,11 @@
 // src/routes/index.ts
 import { Router } from 'express';
-import { createEmployee, getAllEmployees, getEmployeeByID, updateEmployee, deleteEmployee, getSupervisors } from '../controllers/employee.controller';
+import { createEmployee, getAllEmployees, getEmployeeByID, updateEmployee, deleteEmployee } from '../controllers/employee.controller';
 
 const router = Router();
 
 router.post('/', createEmployee);
 router.get('/', getAllEmployees);
-router.get('/supervisor/',getSupervisors);
 router.get('/:id', getEmployeeByID);
 router.put('/:id', updateEmployee);
 router.delete('/:id', deleteEmployee);
