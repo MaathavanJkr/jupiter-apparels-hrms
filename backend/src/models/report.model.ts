bb32682ffdae720fc317884409db5dd9beb067a6
MaathavanJkr <maathumaathavan@gmail.com>
2024-10-22 23:48:51 +0530

    report by group

--- backend/src/models/report.model.ts
+++ backend/src/models/report.model.ts
@@ -2,6 +2,7 @@ import { RowDataPacket } from "mysql2";
 import db from "../database/database";
 import { Output } from "./output.model";
 import { Employee } from "./employee.model";
+import { validateDateString } from "../utils/validateData";
 
 export interface TotalLeaves extends RowDataPacket {
   dept_id: string;
@@ -9,18 +10,23 @@ export interface TotalLeaves extends RowDataPacket {
   total_leaves: number;
 }
 
+export const reportGroups = ["Department", "JobTitle", "PayGrade"];
+
 export const getEmployeesByDepartmentIDModel = async (
   dept_id: string
 ): Promise<Output> => {
   try {
     const [result] = await db
       .promise()
-      .query<RowDataPacket[][]>("CALL getAllEmployeesByFilter(?, null, null, null, null)", [dept_id]);
+      .query<RowDataPacket[][]>(
+        "CALL getAllEmployeesByFilter(?, null, null, null, null)",
+        [dept_id]
+      );
     if (Array.isArray(result) && result.length === 0) {
       return { data: null, error: "User not found", message: null };
     } else {
       return {
-        data: (result[0] as Employee[]),
+        data: result[0] as Employee[],
         error: null,
         message: null,
       };
@@ -32,17 +38,57 @@ export const getEmployeesByDepartmentIDModel = async (
 
 export const getTotalLeavesByDepartmentForPeriodModel = async (
   start_date: string,
-  end_date: string,
+  end_date: string
 ): Promise<Output> => {
+  if (
+    !start_date ||
+    !end_date ||
+    !validateDateString(start_date) ||
+    !validateDateString(end_date)
+  ) {
+    return {
+      data: null,
+      error: "Invalid date format. Please use YYYY-MM-DD.",
+      message: null,
+    };
+  }
   try {
     const [result] = await db
       .promise()
-      .query<RowDataPacket[][]>("CALL getTotalLeavesByDepartmentForPeriod(?, ?)", [start_date, end_date]);
+      .query<RowDataPacket[][]>(
+        "CALL getTotalLeavesByDepartmentForPeriod(?, ?)",
+        [start_date, end_date]
+      );
     if (Array.isArray(result) && result.length === 0) {
       return { data: null, error: "User not found", message: null };
     } else {
       return {
-        data: (result[0] as TotalLeaves[]),
+        data: result[0] as TotalLeaves[],
+        error: null,
+        message: null,
+      };
+    }
+  } catch (error) {
+    return { data: null, error, message: "Database Query Failed" };
+  }
+};
+
+export const getReportsByGroupModel = async (
+  groupBy: string
+): Promise<Output> => {
+  if (!reportGroups.includes(groupBy)) {
+    return { data: null, error: "Invalid groupBy parameter", message: null };
+  }
+
+  try {
+    const [result] = await db
+      .promise()
+      .query<RowDataPacket[][]>(`CALL getReportBy${groupBy}()`);
+    if (Array.isArray(result) && result.length === 0) {
+      return { data: null, error: "User not found", message: null };
+    } else {
+      return {
+        data: result[0] as TotalLeaves[],
         error: null,
         message: null,
       };
