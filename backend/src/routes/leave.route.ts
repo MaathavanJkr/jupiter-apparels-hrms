eb637c267811dac87ee06c827ae626631a7fa61d
Maathavan <maathumaathavan@gmail.com>
2024-10-30 14:51:15 +0530

    leave route fix (#88)

--- backend/src/routes/leave.route.ts
+++ backend/src/routes/leave.route.ts
@@ -5,20 +5,29 @@ import {
     getLeaveApplicationsByEmployeeID,
     getLeaveApplicationByID, updateLeaveApplication,
     applyLeave,
-    getMyLeaveApplications
+    getMyLeaveApplications,
+    getAllLeaveApplications,
+    deleteLeaveApplication,
+    getLeaveApplicationsForSupervisor
 } from '../controllers/leaveapplication.controller';
 import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
+router.post("/", createLeaveApplication);
+router.get("/", getAllLeaveApplications);
+router.get("/:id", getLeaveApplicationByID);
+router.put("/:id", updateLeaveApplication);
+router.delete("/:id", deleteLeaveApplication);
+
+router.get("/super/:id", getLeaveApplicationsForSupervisor);
+
 // Leave application routes
-router.post('/application/', createLeaveApplication); // Create a leave application
 router.post('/apply', userAuth, applyLeave); //get employee leaves by employee id
 router.get('/my', userAuth, getMyLeaveApplications); 
 router.get('/employee/:employee_id', getLeaveApplicationsByEmployeeID); //get employee leaves by employee id
 router.get('/view/:application_id', getLeaveApplicationByID); // get a specific leave application for supervisor to view
 router.put('/reject/:application_id', updateLeaveApplication); // reject leaves
 router.put('/approve/:application_id', updateLeaveApplication); // approve leaves
-
 
 export default router;
